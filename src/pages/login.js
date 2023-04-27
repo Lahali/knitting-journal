77cafaf8a259eb885e07cbb7dd42b6e2abff00cf
Halimeh <101599560+Lahali@users.noreply.github.com>
2023-04-27 19:55:37 +0200

    Fixed forms and added react hook form

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,23 +1,31 @@
 import InputGroup from "@component/components/InputGroup"
 import Link from "next/link"
+import { useForm } from "react-hook-form"
 
 const Login = () => {
+  const { register } = useForm()
   return (
     <div className="flex items-center justify-center h-screen">
       <form className="flex flex-col items-center justify-center bg-[#FFEEE7] py-5 w-4/6 rounded-lg">
         <h1 className="my-4 text-5xl">Signin</h1>
-        <InputGroup
-          placeholder="email"
-          type="email"
-          name="Your email"
-          id="email"
-        />
-        <InputGroup
-          placeholder="password"
-          type="password"
-          name="Password"
-          id="password"
-        />
+        <div className="flex flex-col w-4/6 gap-1 my-5">
+          <label htmlFor="email">Write your email</label>
+          <input
+            placeholder="Email"
+            type="email"
+            className="bg-[#D9D9D9] p-3 rounded"
+            {...register("email")}
+          />
+        </div>
+        <div className="flex flex-col w-4/6 gap-1 my-5">
+          <label htmlFor="password">Write your password</label>
+          <input
+            placeholder="Password"
+            type="password"
+            className="bg-[#D9D9D9] p-3 rounded"
+            {...register("password")}
+          />
+        </div>
         <button className="bg-[#A3342C] text-[#FFEEE7] p-3 my-3 rounded w-4/6 hover:bg-[#7C2923]">
           Signin
         </button>
--- src/pages/register.js
+++ src/pages/register.js
@@ -1,24 +1,49 @@
 import InputGroup from "@component/components/InputGroup"
 import Link from "next/link"
+import { useForm } from "react-hook-form"
 
 const Register = () => {
+  const { register } = useForm()
   return (
     <div className="flex items-center justify-center h-screen">
       <form className="flex flex-col items-center justify-center bg-[#FFEEE7] py-5 w-4/6 rounded-lg">
         <h1 className="my-4 text-5xl">Register</h1>
-        <InputGroup placeholder="name" type="text" name="Your name" id="name" />
-        <InputGroup
-          placeholder="email"
-          type="email"
-          name="Your email"
-          id="email"
-        />
-        <InputGroup
-          placeholder="password"
-          type="password"
-          name="Password"
-          id="password"
-        />
+        <div className="flex flex-col w-4/6 gap-1 my-5">
+          <label htmlFor="name">Write your name</label>
+          <input
+            placeholder="Name"
+            type="text"
+            className="bg-[#D9D9D9] p-3 rounded"
+            {...register("name")}
+          />
+        </div>
+        <div className="flex flex-col w-4/6 gap-1 my-5">
+          <label htmlFor="email">Write your email</label>
+          <input
+            placeholder="Email"
+            type="email"
+            className="bg-[#D9D9D9] p-3 rounded"
+            {...register("email")}
+          />
+        </div>
+        <div className="flex flex-col w-4/6 gap-1 my-5">
+          <label htmlFor="password">Write your password</label>
+          <input
+            placeholder="Password"
+            type="password"
+            className="bg-[#D9D9D9] p-3 rounded"
+            {...register("password")}
+          />
+        </div>
+        <div className="flex flex-col w-4/6 gap-1 my-5">
+          <label htmlFor="passwordValidation">Repeat your password</label>
+          <input
+            placeholder="Repeat password"
+            type="password"
+            className="bg-[#D9D9D9] p-3 rounded"
+            {...register("passwordValidation")}
+          />
+        </div>
         <button className="bg-[#A3342C] text-[#FFEEE7] p-3 my-3 rounded w-4/6 hover:bg-[#7C2923]">
           Submit
         </button>
