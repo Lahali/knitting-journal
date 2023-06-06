5d2c63870e09acf731ba557b3a3879afcbb37e74
Halimeh <101599560+Lahali@users.noreply.github.com>
2023-06-06 10:33:38 +0200

    project page

--- src/pages/project/[project].js
+++ src/pages/project/[project].js
@@ -2,7 +2,8 @@ import { doc, getDoc, updateDoc } from "firebase/firestore"
 import React, { useState } from "react"
 import { dataBase } from "../../../lib/firebase"
 import { useRouter } from "next/router"
-import add from "../../assets/icons/addIcon.svg"
+import { HiOutlinePlus, HiOutlineMinus } from "react-icons/hi"
+
 import Image from "next/image"
 
 const Project = ({ singleProject }) => {
@@ -28,32 +29,51 @@ const Project = ({ singleProject }) => {
   }
 
   return (
-    <div className="p-5">
-      <p>{singleProject.title}</p>
-      <p>{singleProject.technique}</p>
-      <p>{singleProject.yarn}</p>
-      <p>{singleProject.needles}</p>
-      <p>{singleProject.notes}</p>
-      <div className="flex flex-col">
-        <h1 className="text-5xl">
-          {singleProject.counter > 0 ? singleProject.counter : counter}
-        </h1>
-        <button
-          onClick={handleIncrease}
-          type="button"
-          name="increase"
-          className="bg-[#A3342C] text-[#FFEEE7] p-3 my-3 rounded w-1/2 hover:bg-[#7C2923]"
-        >
-          +
-        </button>
-        <button
-          onClick={handleDecrease}
-          type="button"
-          name="decrease"
-          className="bg-[#A3342C] text-[#FFEEE7] p-3 my-3 rounded w-1/2 hover:bg-[#7C2923]"
-        >
-          -
-        </button>
+    <div className="flex flex-row p-5 max-w-[400px]">
+      <div className="flex flex-col bg-[#F1D3CC] p-6 rounded-md">
+        <div className="flex flex-col items-center justify-center">
+          <div className="flex justify-center bg-[#FFEEE7] p-10 rounded-full  w-[80px] h-[80px] my-4">
+            <h1 className="text-5xl">
+              {singleProject.counter > 0 ? singleProject.counter : counter}
+            </h1>
+          </div>
+          <div className="flex flex-row justify-between">
+            <button
+              onClick={handleIncrease}
+              type="button"
+              name="increase"
+              className="bg-[#A3342C] text-[#FFEEE7] p-3 my-3 rounded w-fit hover:bg-[#7C2923] m-4"
+            >
+              <HiOutlinePlus />
+            </button>
+            <button
+              onClick={handleDecrease}
+              type="button"
+              name="decrease"
+              className="bg-[#A3342C] text-[#FFEEE7] p-3 my-3 rounded w-fit hover:bg-[#7C2923] m-4"
+            >
+              <HiOutlineMinus />
+            </button>
+          </div>
+        </div>
+        <div className="m-5">
+          <p className="my-5 font-bold text-center uppercase">
+            {singleProject.title}
+          </p>
+          <p>
+            <span className="font-bold">Technique:</span>{" "}
+            {singleProject.technique}
+          </p>
+          <p>
+            <span className="font-bold">Yarn:</span> {singleProject.yarn}
+          </p>
+          <p>
+            <span className="font-bold">Needles:</span> {singleProject.needles}
+          </p>
+          <p>
+            <span className="font-bold">Notes:</span> {singleProject.notes}
+          </p>
+        </div>
       </div>
     </div>
   )
